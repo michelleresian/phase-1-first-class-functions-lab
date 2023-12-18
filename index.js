4316107a3676a7e980f28480d8a450a11eacb684
Michelle Resian <you@example.com>
2023-12-18 15:12:51 +0300

    firstclass functions

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
-// Code your solution in this file!
-function receivesAFunction(callback) {
-    // Call the callback function
-    callback();
-  }
-
-  function returnsANamedFunction()
-  {
-    return function NamedFunction(){}
-  }
-
-  function returnsAnAnonymousFunction()
-  {
-    return function(){}
-  }
+// returnFirstTwoDrivers
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+  };
+  
+  // returnLastTwoDrivers 
+  const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+  };
+  
+  // array of functions 
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  
+  // FareMultiplier
+  const createFareMultiplier = function(integer) {
+    return function(fare) {
+      return fare * integer;
+    };
+  };
+  
+  // fareDoubler 
+  const fareDoubler = createFareMultiplier(2);
+  
+  // fareTripler
+  const fareTripler = createFareMultiplier(3);
+  
+  // selectDifferentDrivers 
+  const selectDifferentDrivers = function(drivers, fctn) {
+    return fctn(drivers);
+  };
